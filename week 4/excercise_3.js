ad502397d76ee17af793bce10600e55e52d39513
shahraja <@shahraja1202@gmail.com>
2018-11-06 14:29:26 +0700

    excercise_3

--- week 4/excercise_3.js
+++ week 4/excercise_3.js
@@ -1,31 +1,26 @@
-var nama = 'raja';
-var peran = 'Tabib';
-var umur = 12;
+var nama = 'Raja';
+var peran = 'Ksatria';
+var umur = 17;
 
 if (umur < 10) {
   console.log("Maaf adik kecil, tapi umurmu tidak mencukupi untuk memainkan game ini");
-}
-else if (umur >= 35) {
-  console.log("Dengan penuh hormat, orang tua, game ini hanya untuk pemuda pemudi ");
-}
-else {
-  console.log("Selamat datang di game ini " + nama);
-}
-if (nama) {
-  console.log("Hallo " + nama);
-}
-else {
-  console.log("Nama gaboleh kosong!");
-}
-if (peran === 'Ksatria') {
-  console.log("kamu dapat menyerang dengan senjatamu!");
-}
-else if (peran === 'Tabib') {
-  console.log("kamu akan membantu temanmu yang terluka.");
-}
-else if (peran === 'Penyihir') {
-  console.log("ciptakan keajaiban yang membantu kemenanganmu!");
-}
-else{
-  console.log("Pilih peranmu untuk memulai game!");
+}else {
+  if (umur <= 35) {
+    if (nama !== '') {
+      console.log("Selamat datang di Dunia Proxytia " + nama);
+      if (peran === 'Ksatria') {
+        console.log('Hallo Ksatria ' + nama + 'Kamudapat menyerang dengan senjatamu!');
+      }else if(peran === 'Tabib') {
+      console.log('Hallo  Tabib' + nama + 'kamu akan membantu temanmu yang terluka.');
+    }else if(peran === 'Penyihir') {
+      console.log('Hallo  Penyihir' + nama + 'ciptakan keajaiban yang membantu kemenanganmu!');
+    }else {
+      console.log('Pilih peranmu untuk memulai game!');
+    }
+    }else {
+      console.log("Nama tidak boleh kosong");
+    }
+  }else {
+   console.log("Dengan penuh hormat, orang tua, game ini hanya untuk pemuda pemudi "); 
+  }
 }
