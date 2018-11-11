022cc05bf389e2008550414be751b390616652ae
shahraja <@shahraja1202@gmail.com>
2018-11-11 20:29:33 +0700

    [excercise_5_challenge_1.md].js

---
+++ week 4/[exercise_5_challenge_4.md].js
@@ -0,0 +1,10 @@
+function xo(str) {
+ return str.split('o').length-1 !== str.split('x').length-1 ? false : true;
+}
+
+// TEST CASES
+console.log(xo('xoxoxo')); // true
+console.log(xo('oxooxo')); // false
+console.log(xo('oxo')); // false
+console.log(xo('xxxooo')); // true
+console.log(xo('xoxooxxo')); // true
