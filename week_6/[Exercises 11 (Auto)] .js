7c8ffd1049534fd885478cc46e1f6e4fae81abbf
shahraja <@shahraja1202@gmail.com>
2018-11-23 15:16:09 +0700

    memberikan penjelasan dalam exercises 11

--- week_6/[Exercises 11 (Auto)] .js
+++ week_6/[Exercises 11 (Auto)] .js
@@ -1,13 +1,14 @@
 function tentukanDeretAritmatika(arr) {
   // you can only write your code here!
- var cek = true; 
+ var cek = true; 	//membuat variable cek dengan nilai true
   for (var i=0; i<arr.length-2; i++ ){ // for-nya dibatesin jadi kurang 2 indeks, karena kondisi if i+1 i+2 
-        if ((arr[i+1]-arr[i]) === (arr[i+2]-arr[i+1])){
-      } else {
-       cek=false;  
+        if ((arr[i+1]-arr[i]) === (arr[i+2]-arr[i+1])){	
+       //jika variable arr dengan nilai i ditambah 1 dikurangi variable arr i sama dengan variable arr i ditambah 2 dikurang arr i ditambah 1
+      } else { //jika tidak
+       cek=false;  //variable cek bernilai false
     } 
   }
-  return cek;
+  return cek;	//jalankan variable cek
 }
 
 // TEST CASES
