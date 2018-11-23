884009047d0c507d867035892478775d36496e87
shahraja <@shahraja1202@gmail.com>
2018-11-23 14:03:11 +0700

    exercises 6

--- week_6/[Exercises 6 (Auto)].js
+++ week_6/[Exercises 6 (Auto)].js
@@ -3,10 +3,10 @@ function angkaPalindrome(num){
     return num+1    //akan me-return num (parameter) di tambah dengan 1
   }else{    // jika salah
     for(var j = num; j < 2000; j++){    //akan menjalankan variable j yang berisi variable num (parameter)
-      var x = j.toString()
-      x = x.split("")
-      x = x.reverse()
-      x = x.join("")
+      var x = j.toString()  //untuk mengembalikan objek string yang mewakili nilai integer pada variable x
+      x = x.split("")   //x.split("") untuk memecah nilai dan menjadikan nilai ke array
+      x = x.reverse()   //x.reverse() untuk membalikan nilai
+      x = x.join("")    //x.join("") untuk menggabungkan nilai menjadi satu kembali
       
       if (j == x){    //jika nilai j sama dengan nilai x
        return j   //akan me-return nilai j
