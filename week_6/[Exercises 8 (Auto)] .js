1306c9254dd387c3733ce2682caf43271a59d7ae
shahraja <@shahraja1202@gmail.com>
2018-11-23 14:45:24 +0700

    memberikan penjelasan pada exercises 8

--- week_6/[Exercises 8 (Auto)] .js
+++ week_6/[Exercises 8 (Auto)] .js
@@ -1,16 +1,19 @@
 function pasanganTerbesar(num) {
   // you can only write your code here!
-    var arr = [];
-  var res = 0;
+    var arr = [];   //membuat variable arr dengan nilai array
+  var res = 0;    //mambuat variable res dengan nilai 0
   for (var i = 0; i <= num.toString().length-1; i++) {
+    //mengulang variable i dengan nilai 0, i kurang dari sama dengan (parameter) num.toString() .length-1 untuk menjumlahkan dan dikurangi 1, i++ untuk menghitung dari kecil kebesar
     arr.push(num.toString()[i] + num.toString()[i+1]);
+    //variable arr .push() untuk menyimpan nilai
   }
   
-  res =  arr.reduce(function(x,y) {
-    return (x > y) ? x : y;
+  res =  arr.reduce(function(x,y) { 
+  //variable res akan memanggil variable arr .reduce untuk menggabungkan dari jumlah nilai dalam array
+    return (x > y) ? x : y;   //akan me-return nilai x lebih dari y maka x dibagi y
   });
   
-  return Number(res);
+  return Number(res); // jalankan Number dengan parameter res
 }
 
 // TEST CASES
