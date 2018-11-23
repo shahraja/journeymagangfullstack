5437ba9ae0502d56f8b46b836e293db37e3fc0b9
shahraja <@shahraja1202@gmail.com>
2018-11-23 15:50:16 +0700

    memberikan penjelasan dalam exercises 13

--- week_6/[Exercises 13 (Auto)] .js
+++ week_6/[Exercises 13 (Auto)] .js
@@ -1,23 +1,23 @@
 function targetTerdekat(arr) {
   // you can only write your code here!
-  var indexO = arr.indexOf("o")
-  var jarak = 0
-  var jarakTerdekat = arr.length
+  var indexO = arr.indexOf("o") //membuat variable indexO dengan nilai variable arr .indexOf untuk mengmbalikan index saat memanggil string pertama kali dengan value tertentu
+  var jarak = 0   //membuat variable jarak dengan nilai 0
+  var jarakTerdekat = arr.length  //membuat variable jarakTerdekat dengan nilai variable arr.length
   
-  if (arr.indexOf("x") === -1) {
-    return 0
+  if (arr.indexOf("x") === -1) {  //jika arr.indexOf adalah x sama dengan dikurang 1
+    return 0  //jalankan 0
   }
   
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "x") {
-      jarak = Math.abs(i - indexO)
-      if (jarak < jarakTerdekat) {
-        jarakTerdekat = jarak
+  for (var i = 0; i < arr.length; i++) {  //perulangan variable i dengan nilai 0, i kurang dari variable arr.length, i++
+    if (arr[i] === "x") { //jika variable arr dengan nilai i sama dengan x
+      jarak = Math.abs(i - indexO)  //variable jarak sama dengan Math.abs adalah untuk mengembalikan ilai mutlak i - indexO
+      if (jarak < jarakTerdekat) {  //jika jarak kurang dari jarakTerdekat
+        jarakTerdekat = jarak   //variable jarakTerdekat sama dengan variable jarak
       }
     }
   }
   
-  return jarakTerdekat
+  return jarakTerdekat  //jalankan variable jarakTerdekat
 }
 
 // TEST CASES
