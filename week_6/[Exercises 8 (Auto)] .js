0e280d5b17fb81d2396dcedbe49ddc06b15970aa
shahraja <@shahraja1202@gmail.com>
2018-11-30 10:56:22 +0700

    belum mengerti exercises 8

--- week_6/[Exercises 8 (Auto)] .js
+++ week_6/[Exercises 8 (Auto)] .js
@@ -1,19 +1,41 @@
+// # Logic Challenge - Pasangan Angka Terbesar
+
+// ## Problem
+
+// Diberikan sebuah function pasanganTerbesar(angka) yang menerima 1 parameter berupa angka. 
+// Functiona akan menentukan pasangan dua digit angka mana yang paling besar dan me-return angka tersebut. 
+// Contoh, jika angka adalah 183928, maka function akan me-return 92, pasangan dua digit angka yang paling besar diantara yang lainnya.
+
+// ## Code
+
+// ```JavaScript
+// function pasanganTerbesar(num) {
+//   // you can only write your code here!
+// }
+
+// // TEST CASES
+// console.log(pasanganTerbesar(641573)); // 73
+// console.log(pasanganTerbesar(12783456)); // 83
+// console.log(pasanganTerbesar(910233)); // 91
+// console.log(pasanganTerbesar(71856421)); // 85
+// console.log(pasanganTerbesar(79918293)); // 99
+// ```
+
+
 function pasanganTerbesar(num) {
   // you can only write your code here!
-    var arr = [];   //membuat variable arr dengan nilai array
-  var res = 0;    //mambuat variable res dengan nilai 0
+  var arr = [];   
+  var res = 0;
   for (var i = 0; i <= num.toString().length-1; i++) {
-    //mengulang variable i dengan nilai 0, i kurang dari sama dengan (parameter) num.toString() .length-1 untuk menjumlahkan dan dikurangi 1, i++ untuk menghitung dari kecil kebesar
     arr.push(num.toString()[i] + num.toString()[i+1]);
-    //variable arr .push() untuk menyimpan nilai
   }
   
-  res =  arr.reduce(function(x,y) { 
-  //variable res akan memanggil variable arr .reduce untuk menggabungkan dari jumlah nilai dalam array
-    return (x > y) ? x : y;   //akan me-return nilai x lebih dari y maka x dibagi y
+  res =  arr.reduce(function(x,y) {
+    return (x > y) ? x : y;
   });
   
-  return Number(res); // jalankan Number dengan parameter res
+  return Number(res);
+  
 }
 
 // TEST CASES
