42f5c030fb9709a329bc007f822eb944745fcd56
shahraja <@shahraja1202@gmail.com>
2018-11-30 11:03:15 +0700

    belum mengerti

--- week_6/[Exercises 15] .js
+++ week_6/[Exercises 15] .js
@@ -1,26 +1,26 @@
 function groupAnimals(animals) {
   // you can only write your code here!
     animals.sort();
-  var hewan = [];
-  var huruf = animals[0][0];
-  var kebunBinatang = [];
+  var hewan = [];   //membuat variable hewan dengan nilai array
+  var huruf = animals[0][0];  //membuat variable huruf dengan nilai array 0
+  var kebunBinatang = []; //membuat variable kebunBinatang dengan nilai array
   
   
-  for (var i = 0; i < animals.length; i ++) {
-    if (animals[i][0] === huruf) {
-      hewan.push(animals[i]);
+  for (var i = 0; i < animals.length; i ++) {   //membuat perulangan variable i
+    if (animals[i][0] === huruf) {  //jika variable animals dengan nilai array i dan 0 sama dengan  (identik) variable huruf
+      hewan.push(animals[i]); //maka jalankan variable hewan .push() untuk menyimpan variable animals dengan nilai array
     }
-    else {
-      kebunBinatang.push(hewan);
-      hewan = [];
-      huruf = animals[i][0];
-      hewan.push(animals[i]);
+    else {  //jika tidak 
+      kebunBinatang.push(hewan);  //jalankan kebunBinatang .push() untuk menyimpan variable hewan
+      hewan = []; //variable hewan dengan nilai array
+      huruf = animals[i][0];  //variable huruf dengan nilai variable animals dengan nilai array i dan 0
+      hewan.push(animals[i]); //variable hewan .push() untuk menyimpan nilai variable animals dengan nilai array
     }
-    if (i === animals.length-1) {
-      kebunBinatang.push(hewan);
+    if (i === animals.length-1) { //jika i sama dengan (identik) variable animals
+      kebunBinatang.push(hewan);  //jalankan kebunBinatang .push() untuk menyimpan nilai variable hewan
     }
   }
-  return kebunBinatang;
+  return kebunBinatang; //jalankan variable kebunBinatang
 }
 
 // TEST CASES
